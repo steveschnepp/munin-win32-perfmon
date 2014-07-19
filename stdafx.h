37dbf8183a316e99f28ccef16ccc931d6c1b4d39
Steve Schnepp <steve.schnepp@pwkf.org>
2014-07-19 22:41:32 +0200

    Initial commit

---
+++ stdafx.h
@@ -0,0 +1,25 @@
+// stdafx.h : fichier Include pour les fichiers Include système standard,
+// ou les fichiers Include spécifiques aux projets qui sont utilisés fréquemment,
+// et sont rarement modifiés
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+
+// TODO: faites référence ici aux en-têtes supplémentaires nécessaires au programme
+#include <windows.h>
+#include <tchar.h>
+#include <PDHMsg.h>
+
+// C++ Includes
+#include <string>
+#include <vector>
+
+// Includes for plugins
+#include <Pdh.h>
